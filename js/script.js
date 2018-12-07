a8aa0390283067db5b7284d0ef9482b8b46fcb37
Arezoo <arezoo.naseri@yahoo.com>
2018-12-08 00:21:35 +0100

    Adding menu toggle

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log("Hi there! I'm happy too see you here.");
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("links");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
