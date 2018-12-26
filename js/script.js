bee454ea7f89ce98e4840ce649c1480d8741c4ea
Arezoo <arezoo.naseri@yahoo.com>
2018-12-26 22:13:54 +0100

    Code quality checked

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-console.log("Hi there! I'm happy too see you here.");
+console.log("Hi there! I'm happy too see you here.")
 
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-  var x = document.getElementById("links");
-  if (x.style.display === "block") {
-    x.style.display = "none";
+function myFunction () {
+  var x = document.getElementById('links')
+  if (x.style.display === 'block') {
+    x.style.display = 'none'
   } else {
-    x.style.display = "block";
+    x.style.display = 'block'
   }
 }
